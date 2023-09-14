e8549bc37e46235ac994e4b17d6f9f27a640c2d7
Tamilselvan9585 <144341344+Tamilselvan9585@users.noreply.github.com>
2023-09-14 11:48:49 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,13 +1,58 @@
-def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    else:
-        return False
+class BankAccount:
+    def __init__(self, account_number, account_holder, balance=0):
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self.balance = balance
 
-# Input from the user
-year = int(input("Enter a year: "))
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.balance}")
+        else:
+            print("Invalid amount for deposit.")
 
-if is_leap_year(year):
-    print(f"{year} is a leap year.")
-else:
-    print(f"{year} is not a leap year.")
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.balance}")
+        else:
+            print("Invalid amount for withdrawal or insufficient balance.")
+
+    def get_balance(self):
+        return self.balance
+
+    def __str__(self):
+        return f"Account Number: {self.account_number}\nAccount Holder: {self.account_holder}\nBalance: ${self.balance}"
+
+
+def main():
+    # Create a bank account
+    account1 = BankAccount("12345", "John Doe")
+
+    while True:
+        print("\nBank Account Menu:")
+        print("1. Deposit")
+        print("2. Withdraw")
+        print("3. Check Balance")
+        print("4. Exit")
+
+        choice = input("Enter your choice (1/2/3/4): ")
+
+        if choice == "1":
+            amount = float(input("Enter the deposit amount: "))
+            account1.deposit(amount)
+        elif choice == "2":
+            amount = float(input("Enter the withdrawal amount: "))
+            account1.withdraw(amount)
+        elif choice == "3":
+            balance = account1.get_balance()
+            print(f"Current balance: ${balance}")
+        elif choice == "4":
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+
+if __name__ == "__main__":
+    main()
